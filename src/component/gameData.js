09d7082f15d7ec40f3b218e335faf2551f37c110
AtWritesProg <athervasalunke1805@gmail.com>
2025-09-11 01:12:28 +0530

    New Category and PWA

--- src/component/gameData.js
+++ src/component/gameData.js
@@ -1,4 +1,21 @@
 export const categories = {
+  'Indian Food': [
+    'Ras Malai', 'Dosa', 'Munches', 'Vada Pav', 'Pav Bhaji', 'Momos', 'Cakes', 'Chole Bhature', 'Chai', 'Buttermilk',
+    'Pasta', 'Noodles', 'Samosa', 'Brownie', 'Frankie', 'Medu Vada', 'Dal Bhat', 'Khichdi', 'Kadhi', 'Gavar',
+    'Aloo Ka Paratha', 'Thepla', 'Dhokla', 'Jalebi', 'Pani Puri', 'Maggi', 'Biryani', 'Wafer', 'Coffee Cold', 'Halwa',
+    'Dal Bati', 'French Fries', 'Paneer Chilli', 'Idli', 'Puran Poli', 'Fried Rice', 'Manchurian', 'Pizza', 'Burger', 'Sandwich'
+  ],
+  'Indian Movies': [
+    'Kuch Kuch Hota Hai', 'URI', 'Chak De India', '12th Fail', 'War', 'Sholay', 'Hum Dil De Chuke Sanam', 'Raanj Hona',
+    'Happy New Year', 'Sardat', 'Kai Po Che', 'Kabir Singh', '3 Idiots', 'Bajrangi Bhaijaan', 'Dangal', 'DDLJ', 'Taare Zameen Par',
+    'Rang De Basanti', 'Mother India', 'Munna Bhai MBBS', 'PK', 'Dabangg', 'Devdas', 'Drishyam', 'Chhaava', 'Jawan', 'Stree',
+    'Housefull', 'Singh', 'Baiyaara', 'Coolie', 'My Name is Khan', 'Chhak Up Bud', 'Bhaag Milkha Bhaag', 'Zindagi Na Milegi Dobara'
+  ],
+  'Indian Serials': [
+    'Sacred Games', 'Delhi Crime', 'Scam 1992', 'The Family Man', 'Arya', 'Mumbai Diaries 26/11', 'Rocket Boys', 'Swaragni', 'Maharani', 'Tabbar',
+    'Kyunki Saas Bhi Kabhi Bahu Thi', 'Kahaani Ghar Ghar Kii', 'Balika Vadhu', 'Naagin', 'Kasautii Zindagii Kay', 'Bade Achhe Lagte Hain', 'Pavitra Rishta', 'Saath Nibhaana Saathiya',
+    'Ramayan', 'Mahabharat', 'Taarak Mehta Ka Ooltah Chashmah', 'The Kapil Sharma Show', 'Crime Patrol', 'CID', 'Anupamaa', 'Yeh Rishta Kya Kehlata Hai', 'Kumkum Bhagya', 'Sasural Simar Ka', 'Diya Aur Baati Hum', 'Qubool Hai'
+  ],
   'Food': [
     'Pizza', 'Burger', 'Ice Cream', 'Chocolate', 'Apple', 'Sandwich', 'Pasta', 'Cookie', 'Cake', 'Banana',
     'Coffee', 'Tea', 'Bread', 'Rice', 'Chicken', 'Fish', 'Salad', 'Soup', 'Cheese', 'Orange',
@@ -24,20 +41,10 @@ export const categories = {
     'Kyunki Saas Bhi Kabhi Bahu Thi', 'Kahaani Ghar Ghar Kii', 'Balika Vadhu', 'Naagin', 'Kasautii Zindagii Kay', 'Bade Achhe Lagte Hain', 'Pavitra Rishta', 'Saath Nibhaana Saathiya',
     'Ramayan', 'Mahabharat', 'Taarak Mehta Ka Ooltah Chashmah', 'The Kapil Sharma Show', 'Crime Patrol', 'CID', 'Anupamaa', 'Yeh Rishta Kya Kehlata Hai', 'Kumkum Bhagya', 'Sasural Simar Ka', 'Diya Aur Baati Hum', 'Qubool Hai'
   ],
-  'Indian Cricketers': [
-    'Virat Kohli', 'MS Dhoni', 'Rohit Sharma', 'Hardik Pandya', 'KL Rahul', 'Jasprit Bumrah', 'Ravindra Jadeja', 'Rishabh Pant', 'Shikhar Dhawan', 'Bhuvneshwar Kumar',
-    'Yuzvendra Chahal', 'Mohammed Shami', 'Cheteshwar Pujara', 'Ajinkya Rahane', 'Shreyas Iyer', 'Suryakumar Yadav', 'Ishan Kishan', 'Dinesh Karthik', 'Ravichandran Ashwin', 'Umesh Yadav',
-    'Sachin Tendulkar', 'Rahul Dravid', 'Sourav Ganguly', 'VVS Laxman', 'Anil Kumble', 'Kapil Dev', 'Sunil Gavaskar', 'Virender Sehwag', 'Yuvraj Singh', 'Harbhajan Singh'
-  ],
   'Famous Indian People': [
     'APJ Abdul Kalam', 'Mahatma Gandhi', 'Jawaharlal Nehru', 'Indira Gandhi', 'Subhas Chandra Bose', 'Bhagat Singh', 'Rani Lakshmibai', 'Chandragupta Maurya', 'Akbar', 'Shah Rukh Khan',
     'Amitabh Bachchan', 'Priyanka Chopra', 'Deepika Padukone', 'Aamir Khan', 'Salman Khan', 'AR Rahman', 'Lata Mangeshkar', 'Ratan Tata', 'Mukesh Ambani', 'Sundar Pichai',
     'Satya Nadella', 'Kiran Mazumdar Shaw', 'Mary Kom', 'PV Sindhu', 'Saina Nehwal', 'Milkha Singh', 'Sania Mirza', 'Vishwanathan Anand', 'CV Raman', 'Homi Bhabha'
-  ],
-  'Indian States': [
-    'Maharashtra', 'Karnataka', 'Tamil Nadu', 'Kerala', 'Gujarat', 'Rajasthan', 'Punjab', 'Haryana', 'Uttar Pradesh', 'Madhya Pradesh',
-    'Bihar', 'West Bengal', 'Odisha', 'Jharkhand', 'Chhattisgarh', 'Andhra Pradesh', 'Telangana', 'Assam', 'Tripura', 'Meghalaya',
-    'Manipur', 'Mizoram', 'Nagaland', 'Arunachal Pradesh', 'Sikkim', 'Himachal Pradesh', 'Uttarakhand', 'Jammu and Kashmir', 'Ladakh', 'Goa'
   ],
   'Hollywood Movies': [
     'Avengers', 'Titanic', 'Avatar', 'Star Wars', 'Jurassic Park', 'The Dark Knight', 'Inception', 'Interstellar', 'The Matrix', 'Gladiator',
@@ -94,7 +101,7 @@ export const categories = {
     'Batgirl', 'Robin', 'Nightwing', 'Red Hood', 'Harley Quinn', 'Joker', 'Lex Luthor', 'Catwoman', 'Poison Ivy', 'Two-Face',
     'Penguin', 'Riddler', 'Bane', 'Deathstroke', 'Darkseid', 'Doomsday', 'Brainiac', 'General Zod', 'Sinestro', 'Black Adam'
   ],
-  'Objects': [
+  'Things Around You': [
     'Stapler', 'Charger', 'Mobile Phone', 'Rent', 'Toilet', 'Trophy', 'Remote', 'Massage Chair', 'News Paper', 'Band-Aid',
     'Perfume', 'Comb', 'Footwear', 'Key', 'Nail Cutter', 'Tap', 'Tiles', 'Speaker', 'Video Game', 'Power Bank',
     'Sunglasses', 'Ring', 'Frame', 'Book', 'Vehicle', 'Blanket', 'Lipstick', 'Computer', 'Window', 'Chair'
@@ -104,7 +111,7 @@ export const categories = {
     'Gilli Danda', 'Hide N Seek', 'Stone Paper Scissor', 'Mafia', 'Chor Police', 'Marbles', 'Ghar Ghar', 'Ludo', 'Doctor Doctor',
     'Snakes & Ladders', 'Business', 'Chess', 'PUBG', 'Nadi Paheel', 'Swimming', 'Running', 'Boxing', 'Tennis', 'Badminton', 'Archery'
   ],
-  'Places': [
+  'Common Places': [
     'Temple', 'Movie Theater', 'Village', 'Bihar', 'Restaurant', 'Bank', 'Jail', 'Police Station', 'Hospital', 'Cemetery',
     'Ice Cream Parlour', 'Hotel', 'Airport', 'Paan Shop', 'Tea Stall', 'Office', 'Bus Stop', 'Train Station', 'Car Showroom', 'Beach',
     'Mountains', 'Garden', 'Jewellery Shop', 'Mall', 'Liquor Shop', 'Beer Bar', 'Soda Shop', 'School Government', 'College', 'University'
@@ -115,18 +122,16 @@ export const categories = {
     'Duck', 'Deer', 'Eagle', 'Fish', 'Frog', 'Fox', 'Goat', 'Eel', 'Gorilla', 'Horse',
     'Hamster', 'Rabbit', 'Squirrel', 'Jaguar', 'Crocodile', 'Panther', 'Chameleon', 'Owl', 'Kite', 'Lobster'
   ],
-  'Movies': [
-    'Kuch Kuch Hota Hai', 'URI', 'Chak De India', '12th Fail', 'War', 'Sholay', 'Hum Dil De Chuke Sanam', 'Raanj Hona',
-    'Happy New Year', 'Sardat', 'Kai Po Che', 'Kabir Singh', '3 Idiots', 'Bajrangi Bhaijaan', 'Dangal', 'DDLJ', 'Taare Zameen Par',
-    'Rang De Basanti', 'Mother India', 'Munna Bhai MBBS', 'PK', 'Dabangg', 'Devdas', 'Drishyam', 'Chhaava', 'Jawan', 'Stree',
-    'Housefull', 'Singh', 'Baiyaara', 'Coolie', 'My Name is Khan', 'Chhak Up Bud', 'Bhaag Milkha Bhaag', 'Zindagi Na Milegi Dobara'
+  'Indian Cricketers': [
+    'Virat Kohli', 'MS Dhoni', 'Rohit Sharma', 'Hardik Pandya', 'KL Rahul', 'Jasprit Bumrah', 'Ravindra Jadeja', 'Rishabh Pant', 'Shikhar Dhawan', 'Bhuvneshwar Kumar',
+    'Yuzvendra Chahal', 'Mohammed Shami', 'Cheteshwar Pujara', 'Ajinkya Rahane', 'Shreyas Iyer', 'Suryakumar Yadav', 'Ishan Kishan', 'Dinesh Karthik', 'Ravichandran Ashwin', 'Umesh Yadav',
+    'Sachin Tendulkar', 'Rahul Dravid', 'Sourav Ganguly', 'VVS Laxman', 'Anil Kumble', 'Kapil Dev', 'Sunil Gavaskar', 'Virender Sehwag', 'Yuvraj Singh', 'Harbhajan Singh'
   ],
-  'Indian Food': [
-    'Ras Malai', 'Dosa', 'Munches', 'Vada Pav', 'Pav Bhaji', 'Momos', 'Cakes', 'Chole Bhature', 'Chai', 'Buttermilk',
-    'Pasta', 'Noodles', 'Samosa', 'Brownie', 'Frankie', 'Medu Vada', 'Dal Bhat', 'Khichdi', 'Kadhi', 'Gavar',
-    'Aloo Ka Paratha', 'Thepla', 'Dhokla', 'Jalebi', 'Pani Puri', 'Maggi', 'Biryani', 'Wafer', 'Coffee Cold', 'Halwa',
-    'Dal Bati', 'French Fries', 'Paneer Chilli', 'Idli', 'Puran Poli', 'Fried Rice', 'Manchurian', 'Pizza', 'Burger', 'Sandwich'
-  ]
+  'Indian States': [
+    'Maharashtra', 'Karnataka', 'Tamil Nadu', 'Kerala', 'Gujarat', 'Rajasthan', 'Punjab', 'Haryana', 'Uttar Pradesh', 'Madhya Pradesh',
+    'Bihar', 'West Bengal', 'Odisha', 'Jharkhand', 'Chhattisgarh', 'Andhra Pradesh', 'Telangana', 'Assam', 'Tripura', 'Meghalaya',
+    'Manipur', 'Mizoram', 'Nagaland', 'Arunachal Pradesh', 'Sikkim', 'Himachal Pradesh', 'Uttarakhand', 'Jammu and Kashmir', 'Ladakh', 'Goa'
+  ],
 };
 
 export const formatTime = (seconds) => {
